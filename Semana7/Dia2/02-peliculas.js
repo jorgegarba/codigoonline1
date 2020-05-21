ced7e31aa8f162e345c70cc8a727f053e9f60906
Jorge Garnica B <jorgegarba@gmail.com>
2020-05-20 20:14:51 -0500

    ejercicio peliculas fix img

--- Semana7/Dia2/02-peliculas.js
+++ Semana7/Dia2/02-peliculas.js
@@ -1,34 +1,55 @@
 const dibujarPeliculas = ({ results }) => {
- results.forEach((peli) => {
-  $("#contenedor-peliculas").append(`
-       <div class="carousel-cell">
-        <div class="card">
-         <img src="http://placehold.it/150x150" alt="" class="card-img-top">
-         <div class="card-body">
-          <h4 class="card-title">${peli.original_title}</h4>
-          <p class="card-text">
-           ${peli.overview}
-          </p>
-         </div>
-        </div>
-       </div>`);
- });
+  results.forEach((peli) => {
 
- $('#contenedor-peliculas').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
- });
+    let carouselCell = $(`<div class="carousel-cell"></div>`);
+    let card = $(`<div class="card"></div>`);
+    let imagen = $(`<div class="card-img-top imagen-pelicula"></div>`);
+    imagen.css("background-image", `url("https://image.tmdb.org/t/p/w500${peli.poster_path}")`);
+
+    let cardBody = $(`<div class="card-body"></div>`);
+    let cardTitle = $(`<h4 class="card-title">${peli.title}</h4>`);
+
+    let cardText = $(`<p class="card-text">${peli.overview.substr(0, 50)}</p>`);
+    let leerMas = $(`<span>... Leer Mas</span>`);
+    cardText.append(leerMas);
+
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    card.append(imagen);
+    card.append(cardBody);
+
+    carouselCell.append(card);
+
+    $("#contenedor-peliculas").append(carouselCell);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $('#contenedor-peliculas').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
 }
 
 const traerPeliculas = () => {
- let endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa";
- fetch(endpoint, { method: 'GET', body: null })
-  .then((response) => {
-   response.json().then((data) => {
-    dibujarPeliculas(data);
-   })
-  })
+  let endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es";
+  fetch(endpoint, { method: 'GET', body: null })
+    .then((response) => {
+      response.json().then((data) => {
+        dibujarPeliculas(data);
+      })
+    })
 }
 
 traerPeliculas();
--- Semana7/Dia2/dist/02-peliculas.dev.js
+++ Semana7/Dia2/dist/02-peliculas.dev.js
@@ -3,7 +3,21 @@
 var dibujarPeliculas = function dibujarPeliculas(_ref) {
   var results = _ref.results;
   results.forEach(function (peli) {
-    $("#contenedor-peliculas").append("\n       <div class=\"carousel-cell\">\n        <div class=\"card\">\n         <img src=\"http://placehold.it/150x150\" alt=\"\" class=\"card-img-top\">\n         <div class=\"card-body\">\n          <h4 class=\"card-title\">".concat(peli.original_title, "</h4>\n          <p class=\"card-text\">\n           ").concat(peli.overview, "\n          </p>\n         </div>\n        </div>\n       </div>"));
+    var carouselCell = $("<div class=\"carousel-cell\"></div>");
+    var card = $("<div class=\"card\"></div>");
+    var imagen = $("<div class=\"card-img-top imagen-pelicula\"></div>");
+    imagen.css("background-image", "url(\"https://image.tmdb.org/t/p/w500".concat(peli.poster_path, "\")"));
+    var cardBody = $("<div class=\"card-body\"></div>");
+    var cardTitle = $("<h4 class=\"card-title\">".concat(peli.title, "</h4>"));
+    var cardText = $("<p class=\"card-text\">".concat(peli.overview.substr(0, 50), "</p>"));
+    var leerMas = $("<span>... Leer Mas</span>");
+    cardText.append(leerMas);
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    card.append(imagen);
+    card.append(cardBody);
+    carouselCell.append(card);
+    $("#contenedor-peliculas").append(carouselCell);
   });
   $('#contenedor-peliculas').flickity({
     // options
@@ -13,7 +27,7 @@ var dibujarPeliculas = function dibujarPeliculas(_ref) {
 };
 
 var traerPeliculas = function traerPeliculas() {
-  var endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa";
+  var endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es";
   fetch(endpoint, {
     method: 'GET',
     body: null
